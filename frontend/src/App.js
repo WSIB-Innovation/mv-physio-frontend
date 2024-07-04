fe39b60d502813ef8f5900e1b36eef57ce2dd6ff
ashley-1015 <ashley_c@wsib-innovation.ca>
2024-07-04 10:50:58 -0400

    Import frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={"http://localhost:5000/live-capture"} alt="live-capture" />
+    </div>
+  );
+}
+
+export default App;
