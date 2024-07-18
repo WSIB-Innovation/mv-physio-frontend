fc47c9858d123f0849010261ea1d6dd0689eef76
ashley-1015 <ashley_c@wsib-innovation.ca>
2024-07-18 13:53:17 -0400

    test ci/cd pipeline

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
-import Main from "./pages/Main";
-import Login from "./pages/Login";
-import Video from "./pages/Video";
-import ResultsPage from "./components/ResultsPopup";
 
-const theme = createTheme(); 
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/video" element={<Video />} />
-          <Route path="/results" element={<ResultsPage />} />
-        </Routes>
-      </>
-    </ThemeProvider>
+    <h1>Hello World!</h1>
   );
 }
 
