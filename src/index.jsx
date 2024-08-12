3483d49abebf22f40cbef9ffcb5660e0ce1b864b
helena-wsib <helena@wsib-innovation.ca>
2024-08-12 14:56:08 -0400

    build fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
+import { CamProvider } from './context/camContext';
 
 
 const root = document.getElementById('root');
@@ -12,14 +12,13 @@ ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
       <BrowserRouter>
+      <CamProvider>
+
         <App />
+      </CamProvider>
       </BrowserRouter>
     </AuthContextProvider>
   </React.StrictMode>,
   root
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
